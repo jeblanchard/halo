d448a382350e3bab5ab08dff50978c7ff2acd4d2
Joshua Blanchard <jeblanchard@berkeley.edu>
2023-06-20 13:23:09 -0700

    fixed tmp.o bug in build script

--- scripts/build-os-image.sh
+++ scripts/build-os-image.sh
@@ -5,15 +5,16 @@ gcc_command="gcc -ffreestanding -o {}.o -c -Werror -Wextra -Wall -nostdlib -lgcc
 # shellcheck disable=SC2086
 find source -type f -name "*.c" -exec $gcc_command \;
 
+out_dir=./output
+obj_dir="${out_dir}/object-files"
+
 # Compile kernel entry ASM to an object file
 kernel_entry_asm_filename="./source/boot/kernel_entry.asm"
 kernel_entry_obj_filename="kernel_entry.o"
 kernel_entry_obj_full_filename="${obj_dir}/${kernel_entry_obj_filename}"
-nasm -f elf {kernel_entry_asm_filename} -o ${kernel_entry_obj_full_filename}
+nasm -f elf ${kernel_entry_asm_filename} -o ${kernel_entry_obj_full_filename}
 
 # Move all object files to out_dir
-out_dir=./output
-obj_dir="${out_dir}/object-files"
 mv_command="mv {} ${obj_dir}"
 
 # shellcheck disable=SC2086
@@ -21,6 +22,7 @@ find source -type f -name "*.o" -exec $mv_command \;
 
 # Collect list of all object files to link
 o_files_to_link=""
+kernel_tmp_filename="kernel.tmp.o"
 for full_filename in ${obj_dir}/*
   do
     filename="${full_filename##*/}"
@@ -29,7 +31,9 @@ for full_filename in ${obj_dir}/*
     # We filter out the kernel entry point because
     # we later include it at the beginning of our
     # binary
-    if [ ${filename} != ${kernel_entry_obj_filename} ] && [ ${extension} = "o" ]; then
+    if [ ${filename} != ${kernel_entry_obj_filename} ] &&
+    [ ${filename} != ${kernel_tmp_filename} ] &&
+    [ ${extension} = "o" ]; then
       o_files_to_link="${o_files_to_link} ${full_filename}"
     fi
 
@@ -39,14 +43,14 @@ for full_filename in ${obj_dir}/*
 
 # (for IntelliJ)
 # shellcheck disable=SC2086
-kernel_tmp_filename="${obj_dir}/kernel.tmp.o"
-ld -T NUL -o ${kernel_tmp_filename} -Ttext 0x1000 ${kernel_entry_obj_full_filename} ${o_files_to_link}
+kernel_tmp_full_filename="${obj_dir}/${kernel_tmp_filename}"
+ld -T NUL -o ${kernel_tmp_full_filename} -Ttext 0x1000 ${kernel_entry_obj_full_filename} ${o_files_to_link}
 
 # Take only the machine instruction binary from the kernel's
 # object file.
 bin_dir="${out_dir}/bin"
 kernel_bin_name="${bin_dir}/kernel.bin"
-objcopy -O binary -j .text ${kernel_tmp_filename} ${kernel_bin_name}
+objcopy -O binary -j .text ${kernel_tmp_full_filename} ${kernel_bin_name}
 
 # Compile bootsector ASM to binary file
 boot_sect_asm_filename="./source/boot/boot_sect_kernel_bootstrap.asm"
